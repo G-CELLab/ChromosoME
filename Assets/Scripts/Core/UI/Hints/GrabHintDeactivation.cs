abc0c30eab7ab5d1e6e55a4ab827b1205c8b8ad8
Ben Tangen <ben.tangen0234@gmail.com>
2026-05-28 11:43:26 -0600

    Changed removal of grab hint to trigger when you move the magnesium instead of when the magnesium becomes disabled.

--- Assets/Scripts/Core/UI/Hints/GrabHintDeactivation.cs
+++ Assets/Scripts/Core/UI/Hints/GrabHintDeactivation.cs
@@ -4,11 +4,26 @@ public class GrabHintDeactivation : MonoBehaviour
 {
     [SerializeField] private GameObject magnesium;
 
+    public float distanceFromStartPos;
+
+    private Vector3 magnesiumStartPos;
+
+    void Start()
+    {
+        magnesiumStartPos = magnesium.transform.position;
+    }
+
     void Update()
     {
-        if (!magnesium.activeInHierarchy)
+        if (IsMagnesiumGrabbed())
         {
             Destroy(gameObject);
         }
+    }
+
+    private bool IsMagnesiumGrabbed()
+    {
+        distanceFromStartPos = Vector3.Distance(magnesium.transform.position, magnesiumStartPos);
+        return distanceFromStartPos > 0.01f;
     }
 }
